7dba03673a4d14d1b22fa23a6ec7f7489b3bc43d
Reza-1988 <moha.rezahosseini@gmail.com>
2025-03-31 18:16:29 +0200

    Add an explanation of what query we should write in each section

--- SQL-Projects/1re-initial/queries.sql
+++ SQL-Projects/1re-initial/queries.sql
@@ -1,7 +1,8 @@
--- Section1
+-- Write a command that deletes the following data from the data set:
+-- Section1: All users whose last name contains neither m nor
 delete from users where family not like '%d%' and family not like '%m%';
--- Section2
+-- Section2: Information about all users with the last name mohammadi or Salary equal to 7356, 9701, 2885, 7414 or 3801
 delete from users where family = 'mohammadi' or salary = 7356 or salary = 9701
 or salary = 2885 or salary = 7414 or salary = 3801;
--- Section3
+-- Section3: Information about all users with the last name booazar or a birth year between 1995 and 2000 (from the beginning of 1995 to the end of 2000)
 delete from users where birth_date between '1995-01-01' and '2000-12-30' or family = 'booazar';
