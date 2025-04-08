ed63f244c39fe45c8a2dd48373d8c9b3963f436d
Reza-1988 <moha.rezahosseini@gmail.com>
2025-04-08 19:28:52 +0200

    optimized the queries.

--- SQL-Projects/1re-initial/queries.sql
+++ SQL-Projects/1re-initial/queries.sql
@@ -1,8 +1,19 @@
 -- Write a command that deletes the following data from the data set:
--- Section1: All users whose last name contains neither m nor
-delete from users where family not like '%d%' and family not like '%m%';
+
+-- Section1: All users whose last name contains neither m nor d
+delete from users
+       where family not like '%d%'
+         and family not like '%m%';
+
 -- Section2: Information about all users with the last name mohammadi or Salary equal to 7356, 9701, 2885, 7414 or 3801
-delete from users where family = 'mohammadi' or salary = 7356 or salary = 9701
-or salary = 2885 or salary = 7414 or salary = 3801;
+delete from users
+       where family = 'mohammadi'
+          or salary IN (7356, 9701, 2885, 7414, 3801);
+
 -- Section3: Information about all users with the last name booazar or a birth year between 1995 and 2000 (from the beginning of 1995 to the end of 2000)
-delete from users where birth_date between '1995-01-01' and '2000-12-30' or family = 'booazar';
+delete from users
+       where birth_date between '1995-01-01' and '2000-12-30'
+          or family = 'booazar';
+
+-- Alternative way for section 3:
+-- DELETE FROM users WHERE family = 'booazar' OR YEAR(birth_date) BETWEEN 1995 AND 2000;
